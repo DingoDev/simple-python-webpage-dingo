bfc089a6fcb05d11deea98767093b3f6cbcedd7a
dingo_dev <dingo_sec@protonmail.com>
2017-11-28 22:12:52 +1100

    Ton of routing, database fixes

--- server.py
+++ server.py
@@ -37,20 +37,45 @@ CONFIG = {'port': 5000}
 # DATABASE RELATIONSHIPS
 
 class Article_list(DATABASE.model):
-    __tablename__ = 'Article_list'
+    """ 
+        I originally didn't think it would be necessary to split articles 
+        Then I was like, right articles have categories
+        Easier to set up the DB this way anyways
+    """
+    __tablename__ = 'article_lists'
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
+    list_type = DATABASE.Column(DATABASE.String(), unique=True)
     articles = DATABASE.relationship('Article', backref='article_list')
     
 class Article(DATABASE.model):
-    __tablename__ = 'Article'
-    content = DATABASE.Column(DATABASE.integer, primary_key=True)
+    """
+        So this is supposed to be super simple
+        The content id is how you will find the article via the /article/{id}
+        The content stores your actual article, use it to store the html parts
+        such as <h2>trashy waifus</h2> <img src="your_waifu.jpg"/>
+    """
+    __tablename__ = 'articles'
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
+    content_id = DATABASE.Column(DATABASE.Integer, unique=True, index=True)
+    content = DATABASE.Column(DATABASE.String(5000)) # Change the 5000 to whatever you think is your general article length + styling additions/images
 
 class Role(DATABASE.model):
-    __tablename__ = 'Roles'
+    """
+        If you can be bothered with having multiple types of users
+    """
+    __tablename__ = 'roles'
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
     users = DATABASE.relationship('User', backref='Role')
 
 class User(DATABASE.model):
-    __tablename__ = 'User'
-    username = DATABASE.Column(DATABASE.integer, primary_key=True)
+    """
+        We're going to start off real simple with one user for you, the creator
+        You know the deal though, username and password
+    """
+    __tablename__ = 'users'
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
+    username = DATABASE.Column(DATABASE.Integer, unique=True, index=True)
+    password = DATABASE.Column(DATABASE.Integer)
 
 # GET ROUTING
 
@@ -72,27 +97,65 @@ def serve_article():
     content = articles['123964']
     return render_template("article.html", content=content)
 
+@APPLICATION.route('/article')
+def get_articles():
+    """ TODO """
+    req_header = request.headers
+    no_of_articles = req_header['no_of_articles']
+    index = req_header['index']
+    article_list_type = req_header['article_list']
+    return -1
+
+def search_article():
+    """ TODO """
+    req_header = request.headers
+    keywords = req_header['keywords']
+    return -1
+
 # POST/DELETE FUNCTIONS
 
 def dynamic_routing():
     """ TODO """
     return -1
 
-def get_articles():
-    """ TODO """
-    return -1
-
-def search_article():
-    """ TODO """
-    return -1
-
-@APPLICATION.route('/create', methods=['POST'])
+@APPLICATION.route('/api/article/create', methods=['POST'])
 # login required
 def create_article():
     """ TODO """
     return -1
 
+@APPLICATION.route('/api/article/delete', methods=['DELETE'])
 def delete_article():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/user/create', methods=['POST'])
+def create_user():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/user/delete', methods=['POST'])
+def delete_user():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/user/password', methods=['PUT'])
+def change_password():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/user/role', methods=['PUT'])
+def change_role():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/role/create', methods=['POST'])
+def create_role():
+    """ TODO """
+    return -1
+
+@APPLICATION.route('/api/role/delete', methods=['DELETE'])
+def delete_role():
     """ TODO """
     return -1
 
